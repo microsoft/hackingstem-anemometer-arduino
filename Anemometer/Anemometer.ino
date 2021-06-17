f4016b393296a1e4ac429a6f3c89e11fd92d6be0
damyka <damyka@microsoft.com>
2021-06-17 15:12:57 -0700

    added link to github

--- Anemometer/Anemometer.ino
+++ Anemometer/Anemometer.ino
@@ -12,7 +12,7 @@
 //
 // Comments, contributions, suggestions, bug reports, and feature requests
 // are welcome! For source code and bug reports see:
-// http://github.com/[TODO github path to Hacking STEM]
+// https://github.com/microsoft/hackingstem-anemometer-arduino
 //
 // Copyright Microsoft EDU Workshop - HackingSTEM
 // MIT License terms detailed in LICENSE.txt
